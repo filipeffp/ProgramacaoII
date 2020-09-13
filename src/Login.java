fc3180b7dc5f46641d59a4957953d1c8674206ca
filipeffp <48811200+filipeffp@users.noreply.github.com>
2020-09-13 19:15:20 -0300

    atualização

--- src/Login.java
+++ src/Login.java
@@ -2,5 +2,32 @@
 public class Login {
 	private String senha;
 	private String log;
+	
+	
+	public Login(String senha, String log) {
+		super();
+		this.senha = senha;
+		this.log = log;
+	}
+
+
+	public String getSenha() {
+		return senha;
+	}
+
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+
+	public String getLog() {
+		return log;
+	}
+
+
+	public void setLog(String log) {
+		this.log = log;
+	}
 
 }
