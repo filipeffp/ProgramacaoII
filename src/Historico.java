95f74111525abe1c19cd109147f9dccc3b1f37a6
filipeffp <48811200+filipeffp@users.noreply.github.com>
2020-10-02 17:49:03 -0300

    Classe histórico

--- src/Historico.java
+++ src/Historico.java
@@ -1,4 +1,11 @@
+import java.time.LocalDate;
 
 public class Historico {
+	private ArmaDeFogo;
+	private Municao;
+	private LocalDate dataCarga;
+	private LocalDate dataSaida;
+	private LocalDate dataEmprestimo;
+	private LocalDate dataRetorno;
 
 }
