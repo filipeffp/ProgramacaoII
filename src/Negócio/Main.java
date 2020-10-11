bd8193d66b9b70f46c2b5eab8e661948b1336e88
filipeffp <48811200+filipeffp@users.noreply.github.com>
2020-10-11 17:00:57 -0300

    Main

--- src/Negócio/Main.java
+++ src/Negócio/Main.java
@@ -1,18 +1,71 @@
 package Negócio;
 
 import java.time.LocalDate;
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		Pessoa militar1 = new Militar("filipe", "cpf", "1234");
-		System.out.println(militar1);
+		//inicializar os repositórios
+		Scanner input = new Scanner(System.in);
+		int opcao;
+		do {
+			System.out.println("		[MENU]");
+			System.out.println("1 - Cadastrar Armas de fogo");
+			System.out.println("2 - Cadastrar Munições");
+			System.out.println("3 - Empréstimo de Armas de Fogo");
+			System.out.println("4 - Empréstimo de Munições");
+			System.out.println("5 - Saída definitiva de Armas de fogo");
+			System.out.println("6 - Saída definitiva de Munições");
+			System.out.println("7 - Listar Armas de fogo");
+			System.out.println("8 - Listar Munições");
+			System.out.println("9 - Sair");
+			
+			opcao = input.nextInt();
+			
+		     switch (opcao) {
+		     //Cadastrar Armas de fogo
+		       case 1:
+		    	   //add arma de fogo
+		    	   break;
+		    //Cadastrar Munições
+		       case 2:
+		    	   
+		    	   break;
+		    //Empréstimo de Armas de Fogo
+		       case 3:
+
+		    	   break;
+		    //Empréstimo de munições
+		       case 4:
+		    	   
+		    	   break;
+		    //Saída definitiva de Armas de fogo
+		       case 5:
+		    	   
+		    	   break;
+		    //Saída definitiva de Munições
+		       case 6:
+		    	   
+		    	   break;
+		    //Listar Armas de fogo
+		       case 7:
+		    	   
+		    	   break;
+		    //Listar Munições
+		       case 8:
+		    	   
+		    	   break;
+		    	   
+		    //default
+		       default:
+		    	   break;
+		     }
+
+			
+		} while (opcao!=9);
+			
 		
-		ArmaDeFogoPatrimonio arma1 = new ArmaDeFogoPatrimonio("calibre", "especie", LocalDate.now(), LocalDate.now(), "marca", "modelo", "nrSerie", "status", "patrimonio");
-		System.out.println(arma1);
-		
-		MunicaoPatrimonio municao1 = new MunicaoPatrimonio("calibre", "especie", LocalDate.now(), LocalDate.now(), 50, "lote");
-		System.out.println(municao1);
 	}
 
 }
