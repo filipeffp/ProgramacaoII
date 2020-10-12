96e6a3b9c91655c8de524a017baccf34d8107bba
filipeffp <48811200+filipeffp@users.noreply.github.com>
2020-10-11 21:38:42 -0300

    cadastro de usuarios

--- src/Dados/TesteRepositórios.java
+++ src/Dados/TesteRepositórios.java
@@ -5,26 +5,33 @@ import java.time.LocalDate;
 import Negócio.ArmaDeFogo;
 import Negócio.ArmaDeFogoPatrimonio;
 import Negócio.Militar;
+import Negócio.Usuario;
 
 public class TesteRepositórios {
 
 	public static void main(String[] args) {
-		Militar militar = new Militar("Filipe", "1234", "matrícula");
+		Militar militar1 = new Militar("Filipe", "1234", "matrícula");
 		Militar militar2 = new Militar("Filipe", "1235", "matrícula");
 		
 		ArmaDeFogo arma1 = new ArmaDeFogo(".40", "pistola", LocalDate.now(), LocalDate.now(), "marca", "modelo", "1234", "status");
-		ArmaDeFogo arma2 = new ArmaDeFogoPatrimonio(".40", "pistola", LocalDate.now(), LocalDate.now(), "marca", "modelo", "1234", "status", "7890", militar);
-				
+		ArmaDeFogo arma2 = new ArmaDeFogoPatrimonio(".40", "pistola", LocalDate.now(), LocalDate.now(), "marca", "modelo", "1234", "status", "7890", militar1);
+
+		Usuario usuario1 = new Usuario("nome", "809123", "matricula", "senha", false, true);
+		Usuario usuario2 = new Usuario("nome", "135791", "matricula", "senha", false, true);
+		
 		RepositorioPessoas pessoas = new RepositorioPessoas();
 		RepositorioArmaDeFogo armas = new RepositorioArmaDeFogo();
+		RepositorioUsuarios usuarios = new RepositorioUsuarios();
 		
-		pessoas.cadastrarPessoa(militar); //PESSOA CADASTRADA COM SUCESSO!
-		pessoas.cadastrarPessoa(militar); //PESSOA JÁ CADASTRADA
+		System.out.println("\nPESSOAS-------------");
+		pessoas.cadastrarPessoa(militar1); //PESSOA CADASTRADA COM SUCESSO!
+		pessoas.cadastrarPessoa(militar1); //PESSOA JÁ CADASTRADA
 		pessoas.cadastrarPessoa(militar2);//PESSOA CADASTRADA COM SUCESSO!
 		pessoas.removerPessoa(militar2); //PESSOA REMOVIDA COM SUCESSO!
 		pessoas.removerPessoa(militar2); //PESSOA NÃO CADASTRADA!
 		pessoas.cadastrarPessoa(militar2); //PESSOA REATIVADA COM SUCESSO!
-		
+				
+		System.out.println("\nARMAS-------------");
 		armas.cadastrarArmaDeFogo(arma1); //ARMA CADASTRADA COM SUCESSO!
 		armas.cadastrarArmaDeFogo(arma1); //ARMA JÁ CADASTRADA
 		armas.cadastrarArmaDeFogo(arma2);//ARMA CADASTRADA COM SUCESSO!
@@ -32,7 +39,21 @@ public class TesteRepositórios {
 		armas.removerArmaDeFogo(arma2); //ARMA NÃO CADASTRADA!
 		armas.cadastrarArmaDeFogo(arma2); //ARMA REATIVADA COM SUCESSO!
 		
+		System.out.println("\nUSUARIOS-------------");
+		usuarios.cadastrarUsuario(usuario1); //USUARIO CADASTRADA COM SUCESSO!
+		usuarios.cadastrarUsuario(usuario1); //USUARIO JÁ CADASTRADA
+		usuarios.cadastrarUsuario(usuario2);//USUARIO CADASTRADA COM SUCESSO!
+		usuarios.removerUsuario(usuario2); //USUARIO REMOVIDA COM SUCESSO!
+		usuarios.removerUsuario(usuario2); //USUARIO NÃO CADASTRADA!
+		usuarios.cadastrarUsuario(usuario2); //USUARIO REATIVADA COM SUCESSO!
+		
+		System.out.println("\n-------------");
 		System.out.println(arma1);
+		System.out.println(arma2);
+		System.out.println(militar1);
+		System.out.println(militar2);
+		System.out.println(usuario1);
+		System.out.println(usuario2);
 
 	}
 
--- src/Negócio/Usuario.java
+++ src/Negócio/Usuario.java
@@ -1,49 +1,54 @@
 package Negócio;
 
+public class Usuario extends Militar {
+	private String senha;
+	private String log;
+	private boolean admin = false;
+	private boolean ativa = true;
 
-public class Usuario {
-	protected String senha;
-	protected String log;
-	protected Militar militar;
-	protected boolean admin = false;
-	
-	
-	public Usuario(String senha, Militar militar) {
-		super();
+	public Usuario(String nome, String cpf, String matricula, String senha, boolean admin, boolean ativa) {
+		super(nome, cpf, matricula);
 		this.senha = senha;
-		this.militar = militar;
-		this.log = militar.getCpf();
-	}
-
-
-	public boolean isAdmin() {
-		return admin;
-	}
-
-
-	public void setAdmin(boolean admin) {
 		this.admin = admin;
+		this.ativa = ativa;
+		this.log = cpf;
 	}
-
-
-	public String getSenha() {
-		return senha;
-	}
-
-
-	public void setSenha(String senha) {
-		this.senha = senha;
-	}
-
-
+	
 	public String getLog() {
 		return log;
 	}
-
 
 	public void setLog(String log) {
 		this.log = log;
 	}
 
-
+	public String getSenha() {
+		return senha;
 	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public boolean isAdmin() {
+		return admin;
+	}
+
+	public void setAdmin(boolean admin) {
+		this.admin = admin;
+	}
+
+	public boolean isAtiva() {
+		return ativa;
+	}
+
+	public void setAtiva(boolean ativa) {
+		this.ativa = ativa;
+	}
+
+	@Override
+	public String toString() {
+		return "Usuario [log=" + log + ", admin=" + admin + ", ativa=" + ativa + "]";
+	}
+
+}
